92e9e56446918b33b37a868023477fdcd8288df2
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-02-10 19:06:14 +0600

    assessment section done

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -48,64 +48,54 @@
 
   <main class="container">
 
-  <div class="row mb-2">
-        <div class="col-md-6">
-          <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
-              <h3 class="mb-0">Featured post</h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-              <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">World</strong>
+            <h3 class="mb-0">Featured post</h3>
+            <div class="mb-1 text-muted">Nov 12</div>
+            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="stretched-link">Continue reading</a>
           </div>
-        </div>
-        <div class="col-md-6">
-          <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
-              <h3 class="mb-0">Post title</h3>
-              <div class="mb-1 text-muted">Nov 11</div>
-              <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-              <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            </div>
+          <div class="col-auto d-none d-lg-block">
+            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
           </div>
         </div>
       </div>
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">Design</strong>
+            <h3 class="mb-0">Post title</h3>
+            <div class="mb-1 text-muted">Nov 11</div>
+            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
-
-    <div class="row mb-2 d-flex justify-content-center">
-
-    <div class="col-md-9 m-auto">
-      <div class="row">
-                <div class="col-md-6 col-sm-12 py-4 text-left text-sm-center">
-                  <h1>Take Our Asessment</h1>
-                  <p style="
-                      font-size: 24px;
-                      line-height: 1.5;
-                  ">By filling our assessment, we will let you know your chances of passing the immigration screening process.</p>
-                  <a class="btn btn-outline-secondary free-btn" href="http://127.0.0.1:8000/dreamc/create" style="
-                      height: 57px;
-                      width: 315px;
-                      font-size: 26px;
-                  ">
-                    <i class="fas fa-chevron-circle-right" aria-hidden="true"></i>
-                    Free Assessment
-                  </a>
-                </div>
-                <div class="col-md-5 offset-sm-2 col-sm-12  .col-md-5 offset-md-1">
-                  <img src="/img/form2.png" class="form-img" alt="form picture">
-                </div>
-            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+
+    <div class="row offset-md-1 mb-2 col-md-10 offset-md-1 col-sm-12 assess-section">
+        <div class="col-md-6 col-sm-12 py-4 section-2-content">
+          <h1>Take Our Asessment</h1>
+          <p>By filling our assessment, we will let you know your chances of passing the immigration screening process.</p>
+          <a class="btn btn-outline-secondary  free-btn free-btn-big" href="http://127.0.0.1:8000/dreamc/create">
+            <i class="fas fa-chevron-circle-right" aria-hidden="true"></i>
+            Free Assessment
+          </a>
+        </div>
+        <div class="col-md-5 offset-sm-2 col-sm-12  .col-md-5 offset-md-1">
+          <img src="/img/form2.png" class="form-img" alt="form picture" style="
+              width: 100%;
+              height: auto;
+          ">
+        </div>
+    </div>
 
     <div class="row">
       <div class="col-md-8">
